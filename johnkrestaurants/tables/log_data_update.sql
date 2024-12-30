ce53635d1b6ae587c3d491c54b22006b3b135d16
johnkeown <john.keown@gmail.com>
2024-12-30 17:25:28 -0500

    fix typo

--- johnkrestaurants/tables/log_data_update.sql
+++ johnkrestaurants/tables/log_data_update.sql
@@ -4,5 +4,5 @@ CREATE TABLE `log_data_update` (
     `table_name` varchar(50) NOT NULL,
     `message` varchar(1000) NOT NULL,
     `date_created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT fk_log_data_update_lu_log_entry_type FOREIGN KEY (`log_entry_type_id`) REFERENCES lu_log_entry_types(`id`)
+    CONSTRAINT fk_log_data_update_lu_log_entry_type FOREIGN KEY (`log_entry_type_id`) REFERENCES lu_log_entry_type(`id`)
 );
